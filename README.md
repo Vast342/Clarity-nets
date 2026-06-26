7690b1297ed538247bf24b9cc286ebf437621c09
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2026-06-26 18:36:11 -0400

    First Configurations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Clarity-nets
+
+A repository of Clarity's network files. New network binaries are in releases, with the current best network marked as the latest release. Training configurations using JW1912's Bullet are also provided for main nets starting from cn_031, as well as a recreation of cn_030's training configuration (which was lost to time).
---
+++ cn_030-recreation.rs
@@ -0,0 +1,130 @@
+use bullet_lib::{
+    game::{
+        inputs::{ChessBucketsMirrored, get_num_buckets},
+        outputs::MaterialCount,
+    },
+    nn::{
+        InitSettings, Shape,
+        optimiser::{AdamW, AdamWParams},
+    },
+    trainer::{
+        save::SavedFormat,
+        schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
+        settings::LocalSettings,
+    },
+    value::{ValueTrainerBuilder, loader::DirectSequentialDataLoader},
+};
+
+fn main() {
+    // Arch
+    let hl_size = 1024;
+    #[rustfmt::skip]
+    const BUCKET_LAYOUT: [usize; 32] = [
+        0, 0, 1, 1,
+        2, 2, 3, 3,
+        4, 4, 4, 4,
+        4, 4, 4, 4,
+        5, 5, 5, 5,
+        5, 5, 5, 5,
+        5, 5, 5, 5,
+        5, 5, 5, 5
+    ];
+    const NUM_INPUT_BUCKETS: usize = get_num_buckets(&BUCKET_LAYOUT);
+    const NUM_OUTPUT_BUCKETS: usize = 8;
+
+    let dataset_path = "data_shuffled.bin";
+
+    // Training Hyperparams
+    // Pretrain - Short, Low WDL, High LR (To try later)
+    /*let step1_superbatches = 50;
+    let step1_wdl_schedule = wdl::ConstantWDL { value: 0.2 };*/
+
+    // Training - Long, Medium WDL, Medium LR
+    let superbatches = 500;
+    let wdl_schedule = wdl::LinearWDL { start: 0.3, end: 0.6 };
+    let lr_schedule = lr::CosineDecayLR {
+        initial_lr: 0.001,
+        final_lr: 0.001 * 0.3 * 0.3 * 0.3 * 0.3,
+        final_superbatch: superbatches,
+    };
+    let lr_warmup = 200;
+
+    let mut trainer = ValueTrainerBuilder::default()
+        .dual_perspective()
+        .optimiser(AdamW)
+        .inputs(ChessBucketsMirrored::new(BUCKET_LAYOUT))
+        .output_buckets(MaterialCount::<NUM_OUTPUT_BUCKETS>)
+        .save_format(&[
+            // merge in the factoriser weights
+            SavedFormat::id("l0w")
+                .transform(|store, weights| {
+                    let factoriser = store.get("l0f").values.f32().repeat(NUM_INPUT_BUCKETS);
+                    weights.into_iter().zip(factoriser).map(|(a, b)| a + b).collect()
+                })
+                .round()
+                .quantise::<i16>(256),
+            SavedFormat::id("l0b").round().quantise::<i16>(256),
+            SavedFormat::id("l1w").round().quantise::<i16>(64).transpose(),
+            SavedFormat::id("l1b").round().quantise::<i16>(256 * 64),
+        ])
+        .loss_fn(|output, target| output.sigmoid().squared_error(target))
+        .build(|builder, stm_inputs, ntm_inputs, output_buckets| {
+            // input layer factoriser
+            let l0f = builder.new_weights("l0f", Shape::new(hl_size, 768), InitSettings::Zeroed);
+            let expanded_factoriser = l0f.repeat(NUM_INPUT_BUCKETS);
+
+            // input layer weights
+            let mut l0 = builder.new_affine("l0", 768 * NUM_INPUT_BUCKETS, hl_size);
+            l0.weights = l0.weights + expanded_factoriser;
+
+            // output layer weights
+            let l1 = builder.new_affine("l1", 2 * hl_size, NUM_OUTPUT_BUCKETS);
+
+            // inference
+            let stm_hidden = l0.forward(stm_inputs).screlu();
+            let ntm_hidden = l0.forward(ntm_inputs).screlu();
+            let hidden_layer = stm_hidden.concat(ntm_hidden);
+            l1.forward(hidden_layer).select(output_buckets)
+        });
+
+    let optimiser_params = AdamWParams { beta1: 0.95, ..Default::default() };
+    trainer.optimiser.set_params(optimiser_params);
+
+    let stricter_clipping = AdamWParams { beta1: 0.95, max_weight: 0.99, min_weight: -0.99, ..Default::default() };
+    trainer.optimiser.set_params_for_weight("l0w", stricter_clipping);
+    trainer.optimiser.set_params_for_weight("l0f", stricter_clipping);
+
+    let schedule = TrainingSchedule {
+        net_id: "cn_030-recreation".to_string(),
+        eval_scale: 400.0,
+        steps: TrainingSteps {
+            batch_size: 16_384,
+            batches_per_superbatch: 6104,
+            start_superbatch: 1,
+            end_superbatch: superbatches,
+        },
+        wdl_scheduler: wdl_schedule,
+        lr_scheduler: lr::Warmup { inner: lr_schedule, warmup_batches: lr_warmup },
+        save_rate: 10,
+    };
+
+    let settings = LocalSettings { threads: 12, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
+
+    let dataloader = DirectSequentialDataLoader::new(&[dataset_path]);
+
+    //trainer.load_from_checkpoint("D:/bullet/checkpoints/cn_031-3step-600");
+    trainer.run(&schedule, &settings, &dataloader);
+    //trainer.save_quantised("checkpoints/cn_031-2step.nnue").expect("Failed to save quantised model");
+
+    for fen in [
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+    ] {
+        let eval = trainer.eval(fen);
+        println!("FEN: {fen}");
+        println!("EVAL: {}", 400.0 * eval);
+    }
+}
